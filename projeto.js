a28d5a058f73d5f776d18345c9a8f7956508d6cb
Tiago Queiroz <tiago.dev.queiroz@gmail.com>
2023-06-26 11:27:25 -0300

    .

--- projeto.js
+++ projeto.js
@@ -9,18 +9,59 @@ op.set('views', './views');
 op.use(express.static('public'))
 op.use(bp.urlencoded({extended:false}))
 op.use(bp.json())
+var lanche = []
+var estoque = []
 
 op.get('/',function(req,res){
     res.render('inicio')
+})
+op.get('/lanche',function(req,res){
+    res.render('lanche',{lanche})
+})
+op.post('/lanche',function(req,res){
+    nome = req.body.nome
+    preco = req.body.preco
+    molho1 = req.body.molho1
+    molho2 = req.body.molho2
+    descricao = req.body.ingredientes
+    
+    cast = {
+        "nome": nome,
+        "preco": preco,
+        "molho1": molho1,
+        "molho2": molho2,
+        "descricao": descricao
+    }
+    lanche.push(cast)
+
+    res.render('concluido')
+})
+op.get('/estoque',function(req,res){
+    res.render('estoque',{estoque})
+})
+op.post('/estoque',function(req,res){
+    nome = req.body.nome
+    tipo = req.body.tipo
+    setor = req.body.setor
+    descricao = req.body.descricao
+    
+    cast = {
+        "nome": nome,
+        "tipo": tipo,
+        "setor": setor,
+        "descricao": descricao
+    }
+    estoque.push(cast)
+    res.render('concluido')
 })
 op.get('/cadastro',function(req,res){
     res.render('cadastro')
 })
 op.get('/cadastro/estoque',function(req,res){
-    res.render('estoque')
+    res.render('alterarEstoque')
 })
 op.get('/cadastro/lanche',function(req,res){
-    res.render('lanche')
+    res.render('novoLanche')
 })
 
 op.listen(4090,function(){
